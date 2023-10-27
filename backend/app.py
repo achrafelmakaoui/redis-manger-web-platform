4524d1fdb2e64d8c629560d209fa8b9d896d8721
ABR-X <abderrahmaneaitel@gmail.com>
2023-10-27 17:17:14 +0100

    puting some endpoints

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,50 @@
 import redis
-from flask import Flask
+from flask import Flask, jsonify, redirect, request, render_template, session, url_for
+
 
 app = Flask(__name__)
-redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+#redis_conn = redis.StrictRedis(host='localhost', port=6379, db=0)
+
+
+
+@app.route('/')
+def home():
+    return "hello achraf idiot"
+
+@app.route('/connect')
+def connect():
+    return "connected"
+
+@app.route('/disconnect')
+def disconnect():
+    return "disconnected"
+
+@app.route('/keys', methods=['GET'])
+def get_keys():
+    return "all keys"
+
+@app.route('/keys', methods=['POST'])
+def add_key():
+    return "added successfully!!"
+
+@app.route('/keys/<key_name>', methods=['DELETE'])
+def delete_key(key_name):
+    return key_name
+
+@app.route('/keys/<key_name>', methods=['GET'])
+def get_value(key_name):
+    return (str(key_name) + ": shutup dude!!!")
+
+
+@app.route('/keys/<key_name>', methods=['PUT'])
+def update_key_value(key_name):
+
+    return "updated"
+
+
+
+
+
+if __name__ == "__main__":
+    app.secret_key = "secret123"
+    app.run(debug=True)
